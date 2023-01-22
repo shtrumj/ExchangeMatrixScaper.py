389d1e62f286f10efc32c9384a4e0e413f299aca
shtrumj <shtrumj@gmail.com>
2023-01-22 08:36:06 +0200

    With Flasd Restful

--- request.py
+++ request.py
@@ -1,20 +1,6 @@
-from bs4 import BeautifulSoup
+
 import requests
-#
 BASE = "http://127.0.0.1:5000/"
-#
 response = requests.get(BASE + "exchangecu").json()
-#
-# print(response.json())
-import re
-import requests
 
-# BASE = "https://mail.trot.co.il/owa"
-
-# response = requests.get(BASE)
-# soup = BeautifulSoup(response.content, 'html.parser')
-
-# res= soup(response.text)
-
-# Exchange2019 = (re.search("15.02.\d\d\d\d.\d\d\d",str(soup)).group())
 print(response['exchange2013'])
